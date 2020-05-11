82aaf42786ab071d82b30633f49b8c7230f7382f
ruslan <ruskhas131@mail.ru>
2020-05-11 13:31:49 +0300

    Initial Commit

---
+++ probny_exam/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  probny_exam
+//
+//  Created by ruslan on 11.05.2020.
+//  Copyright © 2020 ruslan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
