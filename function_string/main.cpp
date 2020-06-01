1bfa9413166b37f4debdceded726e38ceccbbd1b
ruslan <ruskhas131@mail.ru>
2020-06-01 16:13:03 +0300

    function_find_string

---
+++ function_string/main.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int nthSubstr(int n, const string& s, const string& p) {
+    string::size_type i = s.find(p); // Find the first occurrence
+
+int j;
+for (j = 1; j < n && i != string::npos; ++j)
+    i = s.find(p, i+1); // Find the next occurrence
+
+if (j == n){
+    cout << i;
+    return(i);
+}
+else
+    return(-1);
+}
+
+int main() {
+string s = "the wind, the sea, the sky, the trees";
+string p = "the";
+nthSubstr(2, s, p);
+    //helpfull
+}
